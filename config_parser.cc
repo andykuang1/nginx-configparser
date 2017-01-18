97ad960543f465a76000a3f0f0017b19b95c8255
Andy Kuang <gnauk.ydna@hotmail.com>
2017-01-17 21:22:30 -0800

    Fixed the bug with unmatched braces (found through unit tests). Included the files that the unit tests used.

--- config_parser.cc
+++ config_parser.cc
@@ -152,6 +152,7 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
   config_stack.push(config);
   TokenType last_token_type = TOKEN_TYPE_START;
   TokenType token_type;
+  int counter = 0;
   while (true) {
     std::string token;
     token_type = ParseToken(config_file, &token);
@@ -194,6 +195,7 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
         // Error.
         break;
       }
+      counter++;
       NginxConfig* const new_config = new NginxConfig;
       config_stack.top()->statements_.back().get()->child_block_.reset(
           new_config);
@@ -203,6 +205,7 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
         // Error.
         break;
       }
+      counter--;
       config_stack.pop();
     } else if (token_type == TOKEN_TYPE_EOF) {
       if (last_token_type != TOKEN_TYPE_STATEMENT_END &&
@@ -210,8 +213,13 @@ bool NginxConfigParser::Parse(std::istream* config_file, NginxConfig* config) {
         // Error.
         break;
       }
+      else if (counter != 0) {
+        printf("There are unmatched braces in the config\n");
+        return false;
+      }
       return true;
-    } else {
+    }
+    else {
       // Error. Unknown token.
       break;
     }
--- config_parser_test.cc
+++ config_parser_test.cc
@@ -1,11 +1,30 @@
 #include "gtest/gtest.h"
 #include "config_parser.h"
 
-TEST(NginxConfigParserTest, SimpleConfig) {
-  NginxConfigParser parser;
-  NginxConfig out_config;
+class NginxConfigParserTest : public ::testing::Test{
+	protected:
+		NginxConfigParser parser;
+  		NginxConfig out_config;
+};
+
+TEST_F(NginxConfigParserTest, SimpleConfig) {
 
   bool success = parser.Parse("example_config", &out_config);
 
   EXPECT_TRUE(success);
+  EXPECT_EQ(out_config.ToString(0), "foo \"bar\";\n");
+
+  bool success2 = parser.Parse("example_config2", &out_config);
+  EXPECT_FALSE(success2);
+}
+
+TEST_F(NginxConfigParserTest, TestInput){
+	bool success = parser.Parse("invalideof", &out_config);
+
+	EXPECT_FALSE(success);
+}
+
+TEST_F(NginxConfigParserTest, TestInput2){
+	bool success = parser.Parse("unmatchedbrace", &out_config);
+	EXPECT_FALSE(success);
 }
